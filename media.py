e4251ad67937c0b03a5173639137194cbf538cca
John Bujak <jtbujak@msn.com>
2017-08-17 13:55:25 -0600

    Add files via upload

---
+++ media.py
@@ -0,0 +1,14 @@
+import webbrowser
+
+
+class Movie():
+    """Class for each of my favorite movies"""
+    def __init__(self, movie_title, poster_image, trailer_youtube):
+        """Initialize move name, poster and trailer attributes"""
+        self.title = movie_title
+        self.poster_image_url = poster_image
+        self.trailer_youtube_url = trailer_youtube
+
+    def show_trailer(self):
+        """Plays movie trailer"""
+        webbrowser.open(self.trailer_youtube_url)
